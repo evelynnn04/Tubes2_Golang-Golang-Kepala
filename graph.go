6d2465f103e81bfb3453fae035d3d623a5f13703
CrystalNoob <69108822+CrystalNoob@users.noreply.github.com>
2024-04-21 20:35:21 +0700

    feat: added Graph

---
+++ graph.go
@@ -0,0 +1,46 @@
+package main
+
+type Vertex struct {
+	Key string
+	Vertices map[string]*Vertex
+}
+
+func NewVertex(key string) *Vertex {
+	return &Vertex{
+		Key:      key,
+		Vertices: map[string]*Vertex{},
+	}
+}
+
+type Graph struct {
+	Vertices map[string]*Vertex
+}
+
+func NewDirectedGraph() *Graph {
+	return &Graph{
+		Vertices: map[string]*Vertex{},
+	}
+}
+
+func (g *Graph) AddVertex(key string) {
+	v := NewVertex(key)
+	g.Vertices[key] = v
+}
+
+func (g *Graph) AddEdge(k1, k2 string) {
+	v1 := g.Vertices[k1]
+	v2 := g.Vertices[k2]
+
+	if v1 == nil || v2 == nil {
+		panic("simpul tidak ada!")
+	}
+
+	if _, ok := v1.Vertices[v2.Key]; ok {
+		return
+	}
+
+	v1.Vertices[v2.Key] = v2
+
+	g.Vertices[v1.Key] = v1
+	g.Vertices[v2.Key] = v2
+}
