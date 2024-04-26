0fbe037bb0cfe039a8e7fdeb6e2c7af2bc884111
riyorax <maximiliansulistiyo@gmail.com>
2024-04-26 17:46:39 +0700

    fix : updated json format

--- src/functions/JSONwriter.go
+++ src/functions/JSONwriter.go
@@ -11,6 +11,7 @@ import (
 
 type Node struct {
 	Name string `json:"name"`
+	ID   string `json:"id,omitempty"`
 }
 
 type Link struct {
@@ -18,28 +19,43 @@ type Link struct {
 	Target string `json:"target"`
 }
 
-type Details struct {
-	Runtime   string `json:"runtime"`
-	TotalPath string `json:"totalpath"`
-}
+type DetailsEntry map[string]string
 
 type Graph struct {
-	Nodes   []Node  `json:"nodes"`
-	Links   []Link  `json:"links"`
-	Details Details `json:"details"`
+	Nodes   []Node         `json:"nodes"`
+	Links   []Link         `json:"links"`
+	Details []DetailsEntry `json:"details"`
 }
 
 func DataIntoJson(paths [][]string, filename string, runtime time.Duration, totalpath string) error {
+
 	runtimeString := runtime.String()
 
-	graph := Graph{
-		Details: Details{
-			Runtime:   runtimeString,
-			TotalPath: totalpath,
-		},
+	details := []DetailsEntry{
+		{"runtime": runtimeString},
+		{"totalpath": totalpath},
 	}
+
+	graph := Graph{
+		Details: details,
+	}
+
 	uniqueNodes := make(map[string]bool)
 	linksSet := make(map[string]struct{})
+
+	var fromTitle, toTitle string
+	if len(paths) > 0 {
+		fromURL, err := url.Parse(paths[0][0])
+		if err != nil {
+			return fmt.Errorf("error parsing from URL: %w", err)
+		}
+		toURL, err := url.Parse(paths[0][len(paths[0])-1])
+		if err != nil {
+			return fmt.Errorf("error parsing to URL: %w", err)
+		}
+		fromTitle = strings.TrimPrefix(fromURL.Path, "/wiki/")
+		toTitle = strings.TrimPrefix(toURL.Path, "/wiki/")
+	}
 
 	for _, path := range paths {
 		for i := 0; i < len(path)-1; i++ {
@@ -55,24 +71,27 @@ func DataIntoJson(paths [][]string, filename string, runtime time.Duration, tota
 			sourceTitle := strings.TrimPrefix(sourceURL.Path, "/wiki/")
 			targetTitle := strings.TrimPrefix(targetURL.Path, "/wiki/")
 
-			if !uniqueNodes[sourceTitle] {
-				uniqueNodes[sourceTitle] = true
-				graph.Nodes = append(graph.Nodes, Node{Name: sourceTitle})
-			}
-			if !uniqueNodes[targetTitle] {
-				uniqueNodes[targetTitle] = true
-				graph.Nodes = append(graph.Nodes, Node{Name: targetTitle})
-			}
+			uniqueNodes[sourceTitle] = true
+			uniqueNodes[targetTitle] = true
 
-			source := sourceURL.String()
-			target := targetURL.String()
-			linkKey := source + "->" + target
-
-			if _, exists := linksSet[linkKey]; !exists {
-				linksSet[linkKey] = struct{}{}
-				graph.Links = append(graph.Links, Link{Source: source, Target: target})
-			}
+			linkKey := sourceTitle + "->" + targetTitle
+			linksSet[linkKey] = struct{}{}
 		}
+	}
+
+	for title := range uniqueNodes {
+		node := Node{Name: title}
+		if title == fromTitle {
+			node.ID = "from"
+		} else if title == toTitle {
+			node.ID = "to"
+		}
+		graph.Nodes = append(graph.Nodes, node)
+	}
+
+	for linkKey := range linksSet {
+		parts := strings.Split(linkKey, "->")
+		graph.Links = append(graph.Links, Link{Source: parts[0], Target: parts[1]})
 	}
 
 	graphJSON, err := json.MarshalIndent(graph, "", "  ")
